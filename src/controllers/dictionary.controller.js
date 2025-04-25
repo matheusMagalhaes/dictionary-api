4e0f3da822a2b31bb42d3b7443bb51a0aef5d4d9
matheus <matheusmagalhaes642@gmail.com>
2025-04-25 13:08:46 -0300

    recfactor: dictionary controller

--- src/controllers/dictionary.controller.js
+++ src/controllers/dictionary.controller.js
@@ -1,17 +1,24 @@
 const axios = require("axios");
+const { addToHistory } = require("../services/history.service");
 
 const getWordDefinition = async (req, res) => {
-    const word = req.params.word;
+  const word = req.params.word;
 
   try {
     const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+  
     res.json(response.data);
   } catch (error) {
-    res.status(404).json({ error: response.data.message });
+    if(error.response && error.response.status === 404){
+        return res.status(404).json({error:"Sorry pal, we couldn't find definitions for the word you were looking for" });
+    }
+
+    console.log(error);
+    res.status(500).json({error: "Internal server error"})
   }
-  
 };
 
+
+
+
 module.exports = { getWordDefinition };
-    // const word = req.param.word;
-    // console.log(word);
