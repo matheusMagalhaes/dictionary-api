c2a7502533a8e6fe3865b9d3e0e12f719f59ea02
matheus <matheusmagalhaes642@gmail.com>
2025-04-26 16:16:49 -0300

    feat: add token verification midd

--- src/middlewares/verifyJwtToken.js
+++ src/middlewares/verifyJwtToken.js
@@ -1,6 +1,11 @@
-import { verify } from "jsonwebtoken";
+const jwt  =  require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -8,11 +13,12 @@ const verifyToken = (req, res, next) => {
   }
 
   try {
-    const decoded = verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDUsIm5hbWUiOiJ0aWEiLCJpYXQiOjE3NDU2NTAyMTd9.XJrBAiD_1Ip51STK3NSIgpoqQMXI0rN0o0iGE0wRh-I");
     req.user = decoded;
+    next();
   } catch (error) {
-    res.status(403), json({ message: "Invalid Token" });
+    console.log(error)
+    res.status(403).json({ message: "Invalid Token" });
   }
 };
-
-export default verifyToken;
+module.exports = verifyToken;
