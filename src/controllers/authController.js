8d9529bb8073c602a91b4cbb3d7bd3172f98a5eb
matheus <matheusmagalhaes642@gmail.com>
2025-04-26 11:50:55 -0300

    refactor: auth routes and controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,43 +10,21 @@ const register = async (req, res) => {
   
   try {   
 
-    const result = await pool.query("INSERT INTO USERS (username) values($1)", [
+    const result = await pool.query("INSERT INTO USERS (username) values($1) RETURNING *", [
       name
     ]);
 
     const user = result.rows[0];
     
-    const token = jwt.sign({ name: user.username }, "secret_key");
+    const token = jwt.sign({ id:user.id , name: user.username}, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDUsIm5hbWUiOiJ0aWEiLCJpYXQiOjE3NDU2NTAyMTd9.XJrBAiD_1Ip51STK3NSIgpoqQMXI0rN0o0iGE0wRh-I");
     res.json({ token });
 
   } catch (error) {
+    console.log(error)
     if (error.code === "23505") {
       res.status(400).json({ error: "User already exists" });
     }
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
-const login = async (req, res) => {
-  const { name } = req.body;
-
-  try {
-    const result = await pool.query("SELECT * FROM USERS WHERE username = $1", [
-      name,
-    ]);
-    const user = result.rows[0];
-
-    if (!user) {
-      return res.status(404).json({ error: "No user was found" });
-    }
-
-    const token = jwt.sign({ name: user.username }, "secret_key");
-    res.json({ token });
-
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ erro: "Internal server error" });
-  }
-};
-
-module.exports = { register, login };
+module.exports = { register };
