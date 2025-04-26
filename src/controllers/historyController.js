07b80cb8a8c9370ba98c8f88cdc544988c9cc8f2
matheus <matheusmagalhaes642@gmail.com>
2025-04-26 16:35:44 -0300

    feat: add historyController

---
+++ src/controllers/historyController.js
@@ -0,0 +1,19 @@
+const pool = require("../database/dictionary_db");
+
+const getDictionaryHistory = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const result = await pool.query(
+      "SELECT WORD, searchedat, user_id FROM word_history WHERE USER_ID = $1 ORDER BY searchedat DESC",
+      [userId]
+    );
+
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = {getDictionaryHistory};
--- src/routes/dictionary.routes.js
+++ src/routes/dictionary.routes.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyJwtToken");
 
-const {getWordDefinition} = require('../controllers/dictionary.controller')
+const { getWordDefinition } = require("../controllers/dictionary.controller");
+const { getDictionaryHistory } = require("../controllers/historyController");
 
-router.get('/definition/:word', verifyToken,getWordDefinition);
+router.get("/definition/:word", verifyToken, getWordDefinition);
+router.get("/history", verifyToken, getDictionaryHistory);
 
 module.exports = router;
