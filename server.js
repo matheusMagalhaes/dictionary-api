406132f65f9d486b0341972f370dceccd639a07e
matheus <matheusmagalhaes642@gmail.com>
2025-04-28 00:09:54 -0300

    style: adding console.log to see if the database is connected with the api

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require("./src/app");
 const port = process.env.port || 3000;
+const pool = require('./src/database/dictionary_db');
+
 
 app.listen(port, () => {
   console.log(`Dictionary API online :) \nlistening on ${port}`);
 });
+pool.query("SELECT 1", (err) =>
+  console.log(err ? "❌ Database conection failed" : "✅ Database Conected!")
+);
